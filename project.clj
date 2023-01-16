df6eef4e3f7cfd6d7ef42d725a6e3bee51005832
Sankara Rameswaran <hello@sankara.me>
2023-01-15 19:26:36 -0800

    Layout changes - bg, font, line-height + Mastodon link fix.

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.10.1"]
                            [ring/ring-devel "1.8.2"]
-                           [compojure "1.6.2"]
+                           [compojure "1.6.3"]
                            [ring-server "0.5.0"]
-                           [cryogen-flexmark "0.1.4"]
-                           [cryogen-core "0.4.0"]
+                           [cryogen-flexmark "0.1.5"]
+                           [cryogen-core "0.4.1"]
                            [environ "1.2.0"]]
             :plugins [[lein-ring "0.12.5"]
                       [lein-environ "1.2.0"]]
